4fc1f29fa3cefdb1548852f8d45f8d4d4bb6d271
Jamie4224 <jamietrip@hotmail.nl>
2024-04-11 20:55:36 +0000

    feat: disable server side bugsnag

--- src/runtime/client/plugin.ts
+++ src/runtime/client/plugin.ts
@@ -9,32 +9,34 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config: RuntimeConfig = useRuntimeConfig()
   const options = enhanceOptions<BrowserConfig>(config.public.bugsnag)
 
-  // we check the internal client to prevent the [bugsnag] Bugsnag.start() was called more than once. Ignoring. error
-  let client: Client | null = (Bugsnag as any)._client
-  if (client === null) {
-    try {
-      client = Bugsnag.start(options)
-    } catch (error) {
-      console.log('[Bugsnag] started in mock mode')
-      return {
-        provide: {
-          bugsnag: mockBugsnag
-        }
-      }
-    }
-  }
+  return {}
 
-  nuxtApp.vueApp.provide('bugsnag-client', client)
-
-  const plugin = Bugsnag.getPlugin('vue')
-
-  if (plugin !== undefined) {
-    nuxtApp.vueApp.use(plugin)
-  }
-
-  return {
-    provide: {
-      bugsnag: client
-    }
-  }
+  // // we check the internal client to prevent the [bugsnag] Bugsnag.start() was called more than once. Ignoring. error
+  // let client: Client | null = (Bugsnag as any)._client
+  // if (client === null) {
+  //   try {
+  //     client = Bugsnag.start(options)
+  //   } catch (error) {
+  //     console.log('[Bugsnag] started in mock mode')
+  //     return {
+  //       provide: {
+  //         bugsnag: mockBugsnag
+  //       }
+  //     }
+  //   }
+  // }
+  //
+  // nuxtApp.vueApp.provide('bugsnag-client', client)
+  //
+  // const plugin = Bugsnag.getPlugin('vue')
+  //
+  // if (plugin !== undefined) {
+  //   nuxtApp.vueApp.use(plugin)
+  // }
+  //
+  // return {
+  //   provide: {
+  //     bugsnag: client
+  //   }
+  // }
 })
