436671949ffcd940bd25cbff0d5bb36a7db913f4
Jamie4224 <jamietrip@hotmail.nl>
2024-04-11 21:57:42 +0000

    feat: disable server side bugsnag

--- src/runtime/client/plugin.ts
+++ src/runtime/client/plugin.ts
@@ -8,24 +8,24 @@ import mockBugsnag from '../utils/mockBugsnag'
 export default defineNuxtPlugin((nuxtApp) => {
   const config: RuntimeConfig = useRuntimeConfig()
   const options = enhanceOptions<BrowserConfig>(config.public.bugsnag)
-
-
+  Bugsnag.start(options)
+  return {}
 
   // we check the internal client to prevent the [bugsnag] Bugsnag.start() was called more than once. Ignoring. error
-  let client: Client | null = (Bugsnag as any)._client
-  if (client === null) {
-    try {
-      client = Bugsnag.start(options)
-    } catch (error) {
-      console.log('[Bugsnag] started in mock mode')
-      return {
-        provide: {
-          bugsnag: mockBugsnag
-        }
-      }
-    }
-  }
-  return {}
+  // let client: Client | null = (Bugsnag as any)._client
+  // if (client === null) {
+  //   try {
+  //
+  //   } catch (error) {
+  //     console.log('[Bugsnag] started in mock mode')
+  //     return {
+  //       provide: {
+  //         bugsnag: mockBugsnag
+  //       }
+  //     }
+  //   }
+  // }
+  // return {}
 
   //
   // nuxtApp.vueApp.provide('bugsnag-client', client)
